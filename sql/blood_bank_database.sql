0ace6d049ed01d053a231fa2baa059ccf949bac8
avdurgesh <47136449+avdurgesh@users.noreply.github.com>
2022-04-30 14:21:19 +0530

    Update blood_bank_database.sql

--- sql/blood_bank_database.sql
+++ sql/blood_bank_database.sql
@@ -58,7 +58,7 @@ primary key(contact_id)
 );
 
 insert into contact_info(contact_address,contact_mail,contact_phone)
-values("Hisar,Haryana(125001)","bloodbank@gmail.com","7056550477");
+values("Lucknow,UP(226001)","bloodbank@gmail.com","9758585757");
 
 
 
